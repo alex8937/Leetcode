08d17e3b23f012a0475d9add29258daec660ea79
Chen Chen <ultraseven8937@gmail.com>
2016-09-20 17:20:11 -0400

    09/20/2016

---
+++ 225. Implement Stack using Queues.cpp
@@ -0,0 +1,33 @@
+class Stack {
+private:
+    queue<int> q1, q2;
+public:
+    // Push element x onto stack.
+    void push(int x) {
+        auto& full = (q1.empty()) ? q2 : q1;
+        full.push(x);
+    }
+
+    // Removes the element on top of the stack.
+    void pop() {
+        auto& full = (q1.empty()) ? q2 : q1;
+        auto& emp = (!q1.empty()) ? q2 : q1;
+        while(full.size() != 1) {
+            int temp = full.front();
+            full.pop();
+            emp.push(temp);
+        }
+        full.pop();
+    }
+
+    // Get the top element.
+    int top() {
+        auto& full = (q1.empty()) ? q2 : q1;
+        return full.back();
+    }
+
+    // Return whether the stack is empty.
+    bool empty() {
+        return q1.empty() && q2.empty();
+    }
+};
