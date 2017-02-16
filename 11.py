36130fd297644e6e9e5155338ce38a956c14f2f2
alex8937 <chen@optym.local>
2017-02-15 21:27:06 -0500

    17/02/15

--- 11.py
+++ 11.py
@@ -21,3 +21,25 @@ def maxArea(height):
         while start < end and height[end] <= h:
             end -= 1
     return area
+
+def maxArea2(height):
+    """
+    :type height: List[int]
+    :rtype: int
+    >>> maxArea2([1,2])
+    1
+    >>> maxArea2([1,2,4,5])
+    4
+    >>> maxArea2([5,2,4,5,2])
+    15
+    >>> maxArea2([1,6,4,2])
+    4
+    """
+    area, start, end = 0, 0, len(height) - 1
+    while start < end:
+        area = max(area, min(height[start], height[end]) * (end - start))
+        if height[start] <= height[end]:
+            start += 1
+        else:
+            end -= 1
+    return area
