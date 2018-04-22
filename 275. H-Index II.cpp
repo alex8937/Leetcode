0060eb0e3ffd74612f7026729c9215db200eab6d
alex8937 <ultraseven8937@gmail.com>
2018-04-22 03:13:58 -0400

    20180422

--- 275. H-Index II.cpp
+++ 275. H-Index II.cpp
@@ -18,3 +18,18 @@ public:
         
     }
 };
+
+class Solution {
+public:
+//ver 2
+    int hIndex(vector<int>& citations) {
+        int len = citations.size();
+        int start = 0, end = len - 1;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(len - mid <= citations[mid]) end = mid;
+            else start = mid + 1;
+        }
+        return (start == end && len - start <= citations[start])? len - start : 0;
+    }
+};
